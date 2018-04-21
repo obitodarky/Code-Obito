231659c287d72336b62d0a1b2d6da3ad5b1d37f9
Harsh Mehta <deadspaceharsh@gmail.com>
2018-04-21 23:04:15 +0530

    small change

--- header-footer-widget/footer.php
+++ header-footer-widget/footer.php
@@ -7,5 +7,5 @@ a{
 
 </style></head>
 		<hr>
-		<div  style='float:right'><table><tr><td valign=top style='text-align:right' colspan=1><a href=/about.html>about</a> | <a href=/help.html>code</a> | <a href=/doc/code-help-videos.html>profile | <a href=/done>login<b>/</b>register</a>  </td></tr></table></div><br>
+		<div  style='float:right'><table><tr><td valign=top style='text-align:right' colspan=1><a href=/about.html>about</a> | <a href=/help.html>code</a> | <a href=/doc/code-help-videos.html>profile | <a href=/done>login/register</a>  </td></tr></table></div><br>
 </html>
--- header-footer-widget/header.php
+++ header-footer-widget/header.php
@@ -1,15 +1,13 @@
 
 <html>
 <head><style type="text/css">
-	a{
+a{
     text-decoration: none;
 }
-a:active{
-    color: -webkit-link;
-}
+
 
 </style></head>
 		<a href=HomePage.php><span style='font-size:200%; font-family: "verdana"'><center>ProjectObito</center></span></a>
-		<div  style='float:right'><table><tr><td valign=top style='text-align:right' colspan=1><a href=/about.html>about</a> | <a href=/help.html>code</a> | <a href=/doc/code-help-videos.html>profile | <a href=/done>login<b>/</b>register</a>  </td></tr></table></div><br>
+		<div  style='float:right'><table><tr><td valign=top style='text-align:right' colspan=1><a href=/about.html>about</a> | <a href=/help.html>code</a> | <a href=/doc/code-help-videos.html>profile | <a href=/done>login/register</a>  </td></tr></table></div><br>
 		<hr>
 </html>
