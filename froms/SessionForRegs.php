82b285ca4dd152381c4bf8b22a6a207609e01a10
Harsh Mehta <deadspaceharsh@gmail.com>
2018-04-28 02:22:53 +0530

    Added Session

--- froms/SessionForRegs.php
+++ froms/SessionForRegs.php
@@ -13,35 +13,32 @@ session_start();
 if($_SERVER['REQUEST_METHOD'] == "POST")
 {
 
-$name= $_POST['userid'];
-
 $email= $_POST['email'];
-
 $password= $_POST['password'];
+$okay=true;
+if(empty($email)){
+	$okay=false;
+	echo "Enter Proper Email <br>";
+}
+if(empty($password)){
+	$okay=false;
+	echo "Enter proper password <br>";
+}
+if($okay){
+
+	$_SESSION['email'] = $email;
+
+	$_SESSION["password"] = $password;
 
 
-$_SESSION["userid"] = $name;
-$_SESSION['email'] = $email;
 
-$_SESSION["password"] = $password;
+	echo "hello " .$_SESSION["email"];
 
 
+	session_unset();
+	session_destroy();
 
-echo "hello" .$_SESSION["userid"];
-
-echo $_SESSION["email"];
-
-
-session_unset();
-session_destroy();
-	if($_SESSION == FALSE)
-	{
-			echo "<br> Session Destroyed";
-	}
-	else
-	{
-		echo"<br> Session not destroyed";
-	}
+}
 }		
 ?>
 	
