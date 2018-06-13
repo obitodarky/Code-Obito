c466b789c3d22e1310fd8e96d0e5332a16fd4b83
Harsh Mehta <deadspaceharsh@gmail.com>
2018-06-13 11:03:42 +0530

    improved cgi script

--- cgi_tester.py
+++ cgi_tester.py
@@ -1,5 +1,13 @@
-'''import cgi
+import cgi
+import os
+import cgitb
 
+cgitb.enable()
 form= cgi.FieldStorage()
 term= form.getvalue('submit')
-print(term)'''
+print(term)
+
+if os.environ['REQUEST_METHOD']== "GET":
+    print "Content-type: text/html"
+    print
+    print("<p>hi<p>")
