fd95be0932d49fa8277d795f4fede0a85055e9e8
Harsh Mehta <deadspaceharsh@gmail.com>
2018-06-18 15:36:11 +0530

    start of the crawler

--- cgi_tester.py
+++ cgi_tester.py
@@ -1,17 +1,21 @@
 #!C:\Python34\python.exe
+__author__ = "Harsh Mehta"
 import cgi
 import os
 
-form= cgi.FieldStorage()
-term= form.getvalue('submit')
+def SubmitCgi():{
+    form= cgi.FieldStorage()
+    term= form.getvalue('submit')
 
-print ("Content-type: text/html");
-print ()
-if (os.environ['REQUEST_METHOD']== 'POST'):{
+    print ("Content-type: text/html");
+    print ()
+    if (os.environ['REQUEST_METHOD']== 'POST'):{
 
-    print(""" <html> <head> </head> <body>
-    Hi
-<body></html>""")    
-    
+        print(""" <html> <head> </head> <body>
+        Hi
+    <body></html>""")    
+        
+    }
 }
+
     
