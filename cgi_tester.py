e8b3655ab380b0f1d22c3990fff08ac93e5ce201
Harsh Mehta <deadspaceharsh@gmail.com>
2018-06-18 10:43:28 +0530

    Changes to python scripy

--- cgi_tester.py
+++ cgi_tester.py
@@ -7,8 +7,3 @@ form= cgi.FieldStorage()
 term= form.getvalue('submit')
 print(term)
 
-if os.environ['REQUEST_METHOD']== "GET":{
-    print ("Content-type: text/html")
-    print ()
-    print("<p>hi<p>")
-}
