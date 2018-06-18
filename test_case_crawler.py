e6d427858b75a12c7fe279f12dc735ebf28eb045
Harsh Mehta <deadspaceharsh@gmail.com>
2018-06-18 16:27:25 +0530

    value getter added

--- test_case_crawler.py
+++ test_case_crawler.py
@@ -6,7 +6,13 @@ from bs4 import BeautifulSoup
 
 def value_getter():
     url = "http://localhost/HomePage.php"
+    source = requests.get(url)
+    code_text= source.text
+    soup= BeautifulSoup(code_text)
+    for val in soup.findAll('iframe', {'id': 'resultview'}):
+        value= val.get('body')
+        return value
 
 if(cgi_tester.cgi()==0):
-
+    print(value_getter())
 
