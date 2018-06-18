025103ff7460704df17b6c7181d9efdc4fe54268
Harsh Mehta <deadspaceharsh@gmail.com>
2018-06-18 15:56:29 +0530

    web crawler added

--- test_case_crawler.py
+++ test_case_crawler.py
@@ -1,3 +1,9 @@
 #!C:\Python34\python.exe
-import cgi_crawler
+import cgi_tester
+print ("Content-type: text/html")
+print ()
+
+if(cgi_crawler.cgi()==0):
+    print("""<html><head></head>
+          <body>Started web crawler </body></html>""")
 
