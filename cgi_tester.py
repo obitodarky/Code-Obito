5f92042996f22aef8a24654f7c777a9c13878cdb
Harsh Mehta <deadspaceharsh@gmail.com>
2018-06-18 16:00:17 +0530

    minor improvements

--- cgi_tester.py
+++ cgi_tester.py
@@ -6,6 +6,8 @@ import os
 form = cgi.FieldStorage()
 term= form.getvalue('submit')
 
+print ("Content-type: text/html")
+print ()
 def cgi():
     if (os.environ['REQUEST_METHOD']== 'POST'):
         return 0
