dfea5dee4b3d82568fbc9fff99acf09f764e21ef
Harsh Mehta <deadspaceharsh@gmail.com>
2018-06-18 16:14:13 +0530

    Started Crawler

--- test_case_crawler.py
+++ test_case_crawler.py
@@ -1,8 +1,12 @@
 #!C:\Python34\python.exe
 import cgi_tester
 import cgi
+import requests
+from bs4 import BeautifulSoup
+
+def value_getter():
+    url = "http://localhost/HomePage.php"
 
 if(cgi_tester.cgi()==0):
-    print("""<html><head></head>
-          <body>Started web crawler </body></html>""")
+
 
