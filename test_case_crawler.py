a24c655f3773fcf88ff597f62add0b810887ab83
Harsh Mehta <deadspaceharsh@gmail.com>
2018-06-18 16:03:21 +0530

    Removed errors in crawler

--- test_case_crawler.py
+++ test_case_crawler.py
@@ -1,9 +1,8 @@
 #!C:\Python34\python.exe
 import cgi_tester
-print ("Content-type: text/html")
-print ()
+import cgi
 
-if(cgi_crawler.cgi()==0):
+if(cgi_tester.cgi()==0):
     print("""<html><head></head>
           <body>Started web crawler </body></html>""")
 
